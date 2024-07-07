f5a2dc42f1e1389a9379f229c77b3f8a46c26385
Walaff Fernandes <walaffonofre@icloud.com>
2024-07-07 12:17:07 -0300

    refactor: improve SignatureProvider test coverage

---
+++ src/provider.test.ts
@@ -0,0 +1,22 @@
+import { describe, it, expect } from "vitest";
+import { SignatureProvider } from "./provider.js";
+import { readFile } from "node:fs/promises";
+import { resolve } from "node:path";
+
+describe("SignatureProvider", async () => {
+    const data = Buffer.from("Test data", "utf8");
+    const signature = await readFile(
+        resolve(__dirname, "..", "certificate.p12")
+    );
+    const passphrase = "1234";
+
+    it("should sign the data and return a SignatureEntity", async () => {
+        const signatureProvider = new SignatureProvider(
+            data,
+            signature,
+            passphrase
+        );
+
+        expect(signatureProvider).toBeInstanceOf(SignatureProvider);
+    });
+});
