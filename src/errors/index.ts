dd7c07c5197cc34c40e2d9991cbe8596810c7021
Walaff Fernandes <walaffonofre@icloud.com>
2024-07-07 14:28:34 -0300

    chore: fix import order by errors

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,11 +1,11 @@
 import { CertificateNotFoundError } from "./cert_not_found.error.js";
+import { CertificateExtractionFailedError } from "./certificate_extraction_failed.error.js";
+import { DataNotProvidedError } from "./data_not_provided.error.js";
 import { FilenameNotDefinedError } from "./filename_not_defined.error.js";
 import { FilepathNotDefinedError } from "./filepath_not_defined.error.js";
-import { PrivateKeyNotFoundError } from "./privatekey_not_found.error.js";
-import { DataNotProvidedError } from "./data_not_provided.error.js";
-import { SignatureNotProvidedError } from "./signature_not_provided.error.js";
 import { PrivateKeyExtractionFailedError } from "./privatekey_extraction_failed.error.js";
-import { CertificateExtractionFailedError } from "./certificate_extraction_failed.error.js";
+import { PrivateKeyNotFoundError } from "./privatekey_not_found.error.js";
+import { SignatureNotProvidedError } from "./signature_not_provided.error.js";
 
 const errors = {
     CERT_NOT_FOUND: CertificateNotFoundError,
